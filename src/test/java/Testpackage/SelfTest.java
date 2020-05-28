4f425d2f1ef0e66f08cfb76cf8a5842011b0f9ae
IT-5C <IT-5C@DESKTOP-SL96V9S>
2020-05-28 15:09:44 +0900

    GitHub test spring-oracle

---
+++ src/test/java/Testpackage/SelfTest.java
@@ -0,0 +1,13 @@
+package Testpackage;
+
+public class SelfTest {
+	static int test(int a, int b) {
+		return a+b;
+	}
+	public static void main(String agrs[]) {
+		
+		System.out.println(test(300, 100));
+	}
+	
+	
+}
